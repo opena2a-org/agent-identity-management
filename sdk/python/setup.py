e0063bf669ba5ca396eec80f6ad05a1a4ad1ec57
Abdel Sy Fane <abdelsyfane@gmail.com>
2026-01-02 14:31:53 -0700

    chore: change license from AGPL-3.0 to Apache-2.0
    Switch to Apache-2.0 for better enterprise adoption and SDK embedding.
    Apache-2.0 removes legal friction while keeping the project fully open source.
    
    Updated:
    - LICENSE file
    - package.json, pom.xml, setup.py
    - README files and documentation
    - Swagger annotations

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -28,7 +28,7 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Security :: Cryptography",
-        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
